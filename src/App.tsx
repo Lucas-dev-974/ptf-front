f66fd8c376d3ab479ad671fce73ebdca07762f2c
lucas <lucas.lvn97439@gmail.com>
2024-05-30 08:53:39 +0500

    add page management

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import type { JSXElement } from 'solid-js';
-
+import { Match, Switch, type JSXElement } from 'solid-js';
+import { AppPagesEnum, onPage } from './app.state';
+import { Home } from './views/Home';
 
 export function App(): JSXElement {
-  return <main></main>
+  return <main>
+    <Switch>
+      <Match when={onPage() == AppPagesEnum.home}>
+        <Home />
+      </Match>
+    </Switch>
+  </main>
 }
 
--- src/app.state.ts
+++ src/app.state.ts
@@ -1,1 +1,14 @@
 // * states of app like Type, Enum ...
+
+import { createSignal } from "solid-js";
+
+
+export enum AppPagesEnum {
+    home
+}
+
+export const [onPage, setPage] = createSignal<AppPagesEnum>(AppPagesEnum.home)
+
+export function changePage(page: AppPagesEnum): void {
+    setPage(page)
+}
